7636d64d2b9f7c98deb4bf208426e58644856614
sohi <sohrab.farjami5@gmail.com>
2025-10-28 22:34:56 +0300

    Created Player class and moved player logic

--- Main.java
+++ Main.java
@@ -4,29 +4,46 @@ import java.util.Random;
 
 public class Main{
 	public static void main(String[] args){
+		//Player attackingPlayer = startingPlayer;
+		Player p1 = new Player();
+		Player p2 = new Player();
+
+		Player startingPlayer = getStartingPlayer(p1,p2);
+		Random rand = new Random();
+
 		List<Card> p1Hand = new ArrayList<Card>();
 		List<Card> p2Hand = new ArrayList<Card>();
 		Deck deck = new Deck();
-		Suit[] suits = Suit.values(); 
-		Random rand = new Random();
-		Suit trumpSuit = suits[(rand.nextInt(4))];
-		System.out.printf("The trump suit is %s %n", trumpSuit);
-		dealCards(p1Hand,deck);
+		Card trumpCard = deck.get(rand.nextInt(deck.size()));
+		System.out.printf("The trump card is %d of %s %n", trumpCard.getValue(),trumpCard.getSuit());
+		dealCards(p1Hand,deck,4);
+		dealCards(p2Hand,deck,4);
+
+		System.out.print("Player 1 Hand:");
 		for(Card card : p1Hand){
 			System.out.printf("[%d of %s] ",card.getValue(),card.getSuit());
 		}
 		System.out.println();
+		System.out.print("Player 2 Hand:");
+		for(Card card : p2Hand){
+			System.out.printf("[%d of %s] ",card.getValue(),card.getSuit());
+		}
+		System.out.println();
+		System.out.printf("The starting player is Player %s %n",startingPlayer.getPlayer());
 	}
 
-	static void dealCards(List<Card> hand, Deck deck){
+	static void dealCards(List<Card> hand, Deck deck, int count){
 		Random rand = new Random();
-		System.out.println(deck.size());
-		for(int i = 0; i < 4; i++){
+		for(int i = 0; i < count; i++){
 			int randomIndex = rand.nextInt(deck.size());
 			Card randomElement = deck.get(randomIndex);
 			hand.add(randomElement);
 			deck.remove(randomIndex);
 		}
+	}
+	static Player getStartingPlayer(Player... players){
+		Random rand = new Random();
+		return players[rand.nextInt(players.length)];
 	}
 
 }
---
+++ Player.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player{
+	private static int nextPlayer = 1;
+	private int player;
+	private List<Card> hand = new ArrayList<Card>(4);
+	private List<Card> wonCards = new ArrayList<Card>();
+
+
+	public Player(){
+		player = nextPlayer;
+		nextPlayer++;
+	}
+	public int getPlayer(){
+		return player;
+	}
+
+	public void draw(Card... cards){
+		for(Card card:cards){
+			hand.add(card);
+		}
+
+	}
+
+	public List<Card> getHand(){
+		return hand;
+	}
+
+
+}
