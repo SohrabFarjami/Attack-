ec8f55206c2d7565fa6fbe86cb2e7a54c660d7cd
sohi <sohrab.farjami5@gmail.com>
2025-10-29 23:48:34 +0300

    Changed deck from inheritance to composition

--- Deck.java
+++ Deck.java
@@ -1,21 +1,29 @@
 import java.util.ArrayList;	
 
-public class Deck extends ArrayList<Card>{
+public class Deck{
+	private ArrayList<Card> cards = new ArrayList<Card>();
+
 	public Deck(){
-		super(36);
 		for(Suit suit: Suit.values()){
-			for(int i = 6; i < 14; i++)
-			{
-				this.add(new Card(i,suit));
+			for(int i = 6; i < 14; i++){
+				cards.add(new Card(i,suit));
 			}
 		}
 	}
 
 	public void getDeck(){
-		for(Card card : this){
+		for(Card card : cards){
 			System.out.print(card.getValue());
 			System.out.println(card.getSuit());
 		}
+	}
+
+	public void remove(Card card){
+		cards.remove(card);
+	}
+
+	public void add(Card card){
+		cards.add(card);
 	}
 
 }
